f1d1618443d5cc887370bef6cb3bc900640f429d
devbastian08 <sebastian08.dev@gmail.com>
2023-02-01 00:13:48 -0500

    31 enero

---
+++ index.js
@@ -0,0 +1,5 @@
+var valor = document.getElementById("input");
+var button = document.createElement("encriptar");
+function show (){
+
+document.getElementById("resultado2").style.display = "block";}
